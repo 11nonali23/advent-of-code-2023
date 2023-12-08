807e76575566e7eba2c558c9bbeeec87a94256a5
Andrea Nonali <nonali.andrea@gmail.com>
2023-12-08 01:19:28 +0100

    added question 1 and 2

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import questions.Question1;
+import questions.Question2;
+
+public class Main {
+    public static void main(String[] args) {
+        // ### QUESTION 1
+        // * part 1
+        int part1Result = Question1.getCalibrationValuesSum();
+        System.out.println("Q1 - p1" + part1Result);
+
+        // * part 2
+        int part2Result = Question1.getCalibrationValuesSumWithConvertedNumbers();
+        System.out.println("Q1 - p2" + part2Result);
+
+        // ### QUESTION 2
+        // * part 1
+        long question2Result = Question2.getValidGamesCount();
+        System.out.println("Q2 - p1" + question2Result);
+    }
+
+}
